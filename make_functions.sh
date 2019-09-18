7eb49c435895c6a6ee30dcdec7977e1a3c5a8ed2
aj <alv.js1011@gmail.com>
2019-09-17 22:53:57 -0400

    delete and/or .gitignore make_all.sh, make_functions.sh, make_views.sh, /public. Added etc/js/fallback.min.js to improve load external js, css, etc.

--- make_functions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-printf "\nTranspilate Functions ...\n---------->\n"
-
-./node_modules/.bin/babel src --out-dir functions --ignore "./src/node_modules","./src/view/es6+","./src/view/es5" --presets=@babel/preset-env
