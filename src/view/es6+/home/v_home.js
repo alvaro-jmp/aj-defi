ada32de56037c071ee9832f70acab5e0452325c2
aj <alv.js1011@gmail.com>
2019-09-17 23:15:04 -0400

    testing with fallbackjs

--- src/view/es6+/home/v_home.js
+++ src/view/es6+/home/v_home.js
@@ -8,12 +8,38 @@ class v_home extends Component {
     return (
       div({ class: 'w3-container'}, [
         h1({}, [
-          'Hello world'
+          'aj-bank'
         ])
         ,
         h(test_component)
       ])
     )
+  }
+
+  componentWillMount() {
+
+    // I do this, due to that exists certain fails to load js, css, etc from cdn in Venezuela
+    fallback.load({
+      firebase: [
+        `https://www.gstatic.com/firebasejs/6.6.1/firebase-app.js`,
+        `${window.location.origin}/assets/js/firebase-app-6.6.1.js`
+      ]
+      ,
+      'firebase.auth': [
+        'https://www.gstatic.com/firebasejs/6.6.1/firebase-auth.js',
+        `${window.location.origin}/assets/js/firebase-auth-6.6.1.js`        
+      ]
+      ,
+      'firebase.database': [
+        'https://www.gstatic.com/firebasejs/6.6.1/firebase-database.js',
+        `${window.location.origin}/assets/js/firebase-database-6.6.1.js`        
+      ]
+      ,
+      'firebase.firestore': [
+        'https://www.gstatic.com/firebasejs/6.6.1/firebase-firestore.js',
+        `${window.location.origin}/assets/js/firebase-firestore-6.6.1.js`        
+      ]
+    })
   }
 
   componentDidMount() {
