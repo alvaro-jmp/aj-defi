149d9b70947b5627e179c29ab46c4fa8de9d5fb7
aj <alv.js1011@gmail.com>
2019-09-19 03:11:39 -0400

    modified mini login

--- functions/controller/c_home.js
+++ functions/controller/c_home.js
@@ -1,9 +1,7 @@
 "use strict";
 
 exports.get_router = function (ref_fb, ref_fb_admin, p_get_secret, ref_app) {
-  var renderToString = require("preact-render-to-string");
-
-  var view_home = require('../view/es5/home/v_home')["default"].prototype.render();
+  var view_home = require('../view/es6+/preact_templates/template_v_home');
 
   var express = require('express');
 
@@ -11,19 +9,9 @@ exports.get_router = function (ref_fb, ref_fb_admin, p_get_secret, ref_app) {
 
   var csrf = require('csrf');
 
-  var c_name = 'c_home -->'; // controller name
-
-  var frame_home = require('../view/es5/frame/frame_home');
+  var c_name = 'c_home -->'; // controller name  
 
   var t = require('../lib/tools');
-
-  var send_ssr = function send_ssr(_preact_app_render, _frame, _csrf_tk, res, err) {
-    var fix_tk = typeof err !== 'undefined' ? 'Ogigia' : _csrf_tk;
-
-    var string_html = _frame.print(fix_tk, renderToString(_preact_app_render));
-
-    res.send(string_html);
-  };
 
   router.get(/^(|\/)$/, function (req, res) {
     p_get_secret.then(function (snap) {
@@ -31,11 +19,13 @@ exports.get_router = function (ref_fb, ref_fb_admin, p_get_secret, ref_app) {
       var csrf_tk = csrf().create(secret_tk);
       res.set('Cache-Control', 'public, max-age=86400, s-maxage=86400'); // t.resjson(res, 'Ok !!!')
 
-      send_ssr(view_home, frame_home, csrf_tk, res, undefined);
+      var string_html = view_home.print(csrf_tk);
+      res.send(string_html);
     })["catch"](function (err) {
       t.log2(c_name, "Error in get snaptshot from /secret:", err);
       t.resjson(res, 'with Error !!!', err);
-      send_ssr(view_home, frame_home, csrf_tk, res, err);
+      var string_html = view_home.print('Ogigia');
+      res.send(string_html);
     });
   });
   return router;
