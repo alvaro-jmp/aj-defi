ecdb88c41b8992c9e46c7de5c04ad9273810f586
aj <alv.js1011@gmail.com>
2019-09-19 03:11:39 -0400

    modified mini login

--- src/view/es6+/home/v_home.js
+++ src/view/es6+/home/v_home.js
@@ -1,19 +1,25 @@
 import { Component, h } from 'preact'
 const { div, span, h1, h3, h6, b, a, img, form, label, input, button, br } = require('hyperscript-helpers')(h)
 // import test_component from './components/test_component'
+const w3_margin_top = 16
 
 class v_home extends Component {
 
+  constructor() {
+    super()
+    this.state.height_div_cont = w3_margin_top
+  }
+
   render(props, state) {
     return (
-      div({ class: 'w3-container' }, [
-        div({ class: 'w3-content w3-center', style: { width: '500px' } }, [
-          h1({ class: 'w3-jumbo', style: { 'font-family': '\'Manjari\', sans-serif' } }, ['aj-bank']),
+      div({ ref: c => this.div_cont = c, class: 'w3-display-container w3-margin-top', style: { 'margin-top': this.set_height_div_cont() } }, [
+        div({ ref: c => this.div_login = c, class: 'w3-display-topmiddle w3-card w3-padding custom-card2' }, [
+          h1({ class: 'w3-jumbo w3-center', style: { 'font-family': '\'Manjari\', sans-serif' } }, ['aj-bank']),
           h1({}, ['Email']),
-          input({class: 'w3-input'}, []),
+          input({ class: 'w3-input' }, []),
           h1({}, ['Password']),
-          input({class: 'w3-input'}, []),
-          button({class: 'w3-button w3-block w3-blue w3-hover-teal w3-section w3-padding w3-large', type: 'button'}, ['Sign in'])
+          input({ class: 'w3-input' }, []),
+          button({ class: 'w3-button w3-center w3-block w3-blue w3-hover-teal w3-section w3-padding w3-large', type: 'button' }, ['Sign in'])
         ])
       ])
     )
@@ -47,6 +53,20 @@ class v_home extends Component {
 
   componentDidMount() {
     console.log(`componentDidMount() HEYY !!!! Hello world xD`)
+    this.center_vertically(this.div_cont, this.div_login)
+  }
+
+  center_vertically(parent_elem, elem) {
+    const half_win_height_size = parseInt(window.innerHeight / 2)
+    const half_elem_height_size = parseInt(elem.clientHeight / 2)
+
+    const final_height = half_win_height_size - half_elem_height_size - w3_margin_top
+
+    this.setState({ height_div_cont: final_height })
+  }
+
+  set_height_div_cont() {
+    return `${this.state.height_div_cont}px`
   }
 }
 
