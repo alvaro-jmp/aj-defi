ce1540b4e772144950e006e1a3457f349f274681
aj <alv.js1011@gmail.com>
2019-09-19 03:11:39 -0400

    modified mini login

--- src/controller/c_home.js
+++ src/controller/c_home.js
@@ -1,21 +1,10 @@
-exports.get_router = (ref_fb, ref_fb_admin, p_get_secret, ref_app) => {
-  const renderToString = require("preact-render-to-string")
-  const view_home = require('../view/es5/home/v_home').default.prototype.render()
+exports.get_router = (ref_fb, ref_fb_admin, p_get_secret, ref_app) => {  
+  const view_home = require('../view/es6+/preact_templates/template_v_home')
   const express = require('express')
   const router = express.Router();
   const csrf = require('csrf')
-  const c_name = 'c_home -->' // controller name
-  const frame_home = require('../view/es5/frame/frame_home')
+  const c_name = 'c_home -->' // controller name  
   const t = require('../lib/tools')
-  
-  const send_ssr = (_preact_app_render, _frame, _csrf_tk, res, err) => {
-    const fix_tk = (typeof err !== 'undefined') ? 'Ogigia' : _csrf_tk
-    const string_html = _frame.print(
-      fix_tk,
-      renderToString(_preact_app_render)
-    )
-    res.send(string_html)
-  }
 
   router.get(/^(|\/)$/, (req, res) => {
 
@@ -25,12 +14,14 @@ exports.get_router = (ref_fb, ref_fb_admin, p_get_secret, ref_app) => {
         const csrf_tk = csrf().create(secret_tk)        
         res.set('Cache-Control', 'public, max-age=86400, s-maxage=86400')
         // t.resjson(res, 'Ok !!!')
-        send_ssr(view_home, frame_home, csrf_tk, res, undefined)
+        const string_html = view_home.print(csrf_tk)
+        res.send(string_html)
       })
       .catch((err) => {
         t.log2(c_name, `Error in get snaptshot from /secret:`,err)
         t.resjson(res, 'with Error !!!', err)
-        send_ssr(view_home, frame_home, csrf_tk, res, err)
+        const string_html = view_home.print('Ogigia')
+        res.send(string_html)        
       })
   })
 
