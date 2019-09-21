6f5501c864ef3e5b2e842396adbd4732ade23a89
aj <alv.js1011@gmail.com>
2019-09-21 01:03:50 -0400

    fixing and ordering in folder the scripts and delete todo.js

---
+++ make_views_fun.sh
@@ -0,0 +1,1 @@
+node ./scripts/todo.js && chmod +x make*.sh ./scripts/* && bash -ic "./scripts/make_views.sh && source ./scripts/make_functions.sh"
---
+++ scripts/make_views.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+printf "\nTranspilate Views ...\n---------->\n"
+
+#Delete temp (temporal) folder and create new one and two subfolders
+
+rm -rf ./temp && mkdir -p ./temp/res/js-views
+
+
+#Convert v_home.js to common js incorporating preact for to use in client side rendering and server side rendering
+
+if [ -f ./src/view/es6+/home/index.js ]; then rm -f ./src/view/es6+/home/index.js; fi && cp -v ./src/view/es6+/home/v_home.js ./src/view/es6+/home/index.js && preact build --src ./src/view/es6+/home --dest ./temp/v_home --service-worker false --clean true --no-prerender
+
+if [ -f ./etc/info_licenses_used.txt ] ; then cat ./etc/info_licenses_used.txt | cat - ./temp/v_home/bundle*.js > ./temp/v_home/temp && mv -v ./temp/v_home/temp ./temp/v_home/bundle*.js ; fi 
+
+cp -v ./temp/v_home/bundle*.js ./temp/res/js-views/ && cp -v ./temp/v_home/polyfills*.js ./temp/res/js-views/ 
+
+if [ -f ./src/view/es6+/home/index.js ]; then rm -f ./src/view/es6+/home/index.js; fi && bundle_n_polyfills=$(cat ./temp/v_home/index.html | grep -oE '<body>.+</body>') && ssr=$(node ./scripts/ssr.js) && sed -e "s@<body>.*</body>@$bundle_n_polyfills@" -e "s@/bundle@/assets/js/bundle@" -e "s@/polyfills@/assets/js/polyfills@" -e "s@</script>')@<\\\\\\\\\\\\\/script>')@" -e "s@<body>@<body>$ssr@" ./src/view/es6+/preact_templates/template_v_home.js > ./src/view/es6+/preact_templates/temp && cp -v ./src/view/es6+/preact_templates/temp ./src/view/es6+/preact_templates/template_v_home.js && rm -f ./src/view/es6+/preact_templates/temp
+
+
+
+#Adding static files as css, js, html, fonts to public folder in its respective assets folders
+
+rm -rf public && mkdir -p ./public/assets/js ./public/assets/css ./public/assets/img ./public/assets/fonts && if [ "$(find ./etc/html -type f -name "*.html" 2>/dev/null)" ] ; then cp -v ./etc/html/* ./public/ ; fi && if [ "$(find ./etc/css -type f -name "*.css" 2>/dev/null)" ] ; then cp -v ./etc/css/*.css ./public/assets/css/ ; fi && if [ "$(find ./temp/res/js-views -type f -name "*.js" 2>/dev/null)" ] ; then cp -v ./temp/res/js-views/*.js ./public/assets/js/ ; fi && if [ "$(ls -A ./etc/fonts 2>/dev/null)" ] ; then cp -rv ./etc/fonts/* ./public/assets/fonts/ ; fi && CP_MFT_N_ICON='if [ "$(find ./etc/img -type f -name "manifest.json" 2>/dev/null)" ]; then cp -v ./etc/manifest.json ./public/; fi && if [ "$(find ./etc/img -type f -name "favicon.ico" 2>/dev/null)" ]; then cp -v ./etc/img/favicon.ico ./public/assets/img/; fi' && if [ "$(find ./etc/js -type f -name "*.js" 2>/dev/null)" ] ; then cp -v ./etc/js/*.js ./public/assets/js/ && eval $CP_MFT_N_ICON ; else eval $CP_MFT_N_ICON ; fi
+
+#Delete index.html if exists
+
+rm -rf ./public/index.html
