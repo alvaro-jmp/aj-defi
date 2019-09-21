d9eb35a453fe7453e30ae7d28220023d5a4551bc
aj <alv.js1011@gmail.com>
2019-09-21 01:16:28 -0400

    modified mini login, added validator.min.js and initital_component to use for server side rendering due to problems when use global variables that are not defined in the default window DOM when use preact build prerender for server side rendering.

--- src/view/es6+/home/v_home.js
+++ src/view/es6+/home/v_home.js
@@ -2,62 +2,39 @@ import { Component, h } from 'preact'
 const { div, span, h1, h3, h6, b, a, img, form, label, input, button, br } = require('hyperscript-helpers')(h)
 // import test_component from './components/test_component'
 const w3_margin_top = 16
+const t = require('../lib/tools')
+const { vdom_mini_login } = require('./components/initial_component')
 
 class v_home extends Component {
 
   constructor() {
     super()
     this.state.div_login_margin_top = w3_margin_top
+    this.state.enable_submit_data = true
+    this.state.all_js_loaded = false
   }
 
   render(props, state) {
-    return (
-      div({ ref: c => this.div_cont = c, class: 'w3-display-container', style: { 'margin-top': this.set_div_login_margin_top() } }, [
-        div({ ref: c => this.div_login = c, class: 'w3-display-topmiddle w3-card w3-padding custom-card2 w3-margin-top' }, [
-          h1({ class: 'w3-jumbo w3-center', style: { 'font-family': '\'Manjari\', sans-serif' } }, ['aj-bank']),
-          h1({}, ['Email']),
-          input({ class: 'w3-input', type: 'text', placeholder: 'type your email here', name: 'email', autocomplete: 'email', maxlength: '50' }, []),
-          h1({}, ['Password']),
-          input({ class: 'w3-input', type: 'password', placeholder: 'type your password here', name: 'psw', autocomplete: 'current-password', maxlength: '1024' }, []),
-          button({ class: 'w3-button w3-center w3-block w3-blue w3-hover-teal w3-section w3-padding w3-large', type: 'button' }, ['Sign in'])
-        ])
-      ])
-    )
+    return (vdom_mini_login(this.set_div_login_margin_top()))
   }
 
   componentWillMount() {
-
-    // I do this, due to that exists certain fails to load js, css, etc from cdn in Venezuela
-    // fallback.load({
-    //   firebase: [
-    //     `https://www.gstatic.com/firebasejs/6.6.1/firebase-app.js`,
-    //     `${window.location.origin}/assets/js/firebase-app-6.6.1.js`
-    //   ]
-    //   ,
-    //   'firebase.auth': [
-    //     'https://www.gstatic.com/firebasejs/6.6.1/firebase-auth.js',
-    //     `${window.location.origin}/assets/js/firebase-auth-6.6.1.js`
-    //   ]
-    //   ,
-    //   'firebase.database': [
-    //     'https://www.gstatic.com/firebasejs/6.6.1/firebase-database.js',
-    //     `${window.location.origin}/assets/js/firebase-database-6.6.1.js`
-    //   ]
-    //   ,
-    //   'firebase.firestore': [
-    //     'https://www.gstatic.com/firebasejs/6.6.1/firebase-firestore.js',
-    //     `${window.location.origin}/assets/js/firebase-firestore-6.6.1.js`
-    //   ]
-    // })
+    // load_fallback_js()
   }
 
   componentDidMount() {
-    console.log('componentDidMount() this:', this)
-    
-    this.center_vertically(this.div_cont, this.div_login)
-    
+    console.log('v_home::componentDidMount() this:', this)
+
+    const div_cont = document.querySelector('#div_cont_mini_login')
+    const div_login = document.querySelector('#div_login')
+    const form_min_login = document.querySelector('#form_mini_login')
+
+    form_min_login.addEventListener('submit', this.submit_data)
+
+    this.center_vertically(div_cont, div_login)
+
     window.addEventListener("resize", () => {
-      this.center_vertically(this.div_cont, this.div_login)
+      this.center_vertically(div_cont, div_login)
     })
   }
 
@@ -68,13 +45,40 @@ class v_home extends Component {
     const final_height = half_win_height_size - half_elem_height_size - w3_margin_top
 
     const fixed_final_height = final_height < 0 ? 0 : final_height
-
-    console.log('center_vertically() final_height:', final_height)    
     this.setState({ div_login_margin_top: fixed_final_height })
   }
 
   set_div_login_margin_top() {
-    return `${this.state.div_login_margin_top}px`
+    return `${this.state.div_login_margin_top.toString()}px`
+  }
+
+  submit_data(e) {
+    e.preventDefault()
+    console.log('v_home::submit_data()')
+    if (this.state.enable_submit_data && this.state.all_js_loaded) {
+      const tk = document.getElementsByTagName("META")[4].content
+      const email = document.querySelector('#i_email')
+      const psw = document.querySelector('#i_psw')
+
+      const email_verf = validator.isEmail(email)
+      const psw_verf = t.psw_verf(psw)
+      const tk_verf = t.tk_verf(tk)
+
+      const try_again = () => {
+        this.state.alert_class = 'w3-panel w3-blue w3-display-container'
+        this.state.alert_msg = 'Please try again the login'
+      }
+
+      const show_alert = () => { this.state.show_alert_ok = true }
+
+      console.log('v_home::submit_data() email, psw:', email, psw)
+      console.log('v_home::submit_data() email_verf, psw_verf, tk_verf:', email_verf, psw_verf, tk_verf)
+
+      if (email_verf && psw_verf && tk_verf) {
+        console.log('v_home::submit_data() all fine')
+      }
+
+    }
   }
 }
 
