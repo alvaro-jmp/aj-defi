e44566a7532eb179deb98c719cf6e14d80fec3a7
alvarojmp <alv.js1011@gmail.com>
2019-09-29 15:55:35 -0400

    added support firebase v7.0.0

--- src/view/es6+/lib/fallback_js.js
+++ src/view/es6+/lib/fallback_js.js
@@ -16,6 +16,16 @@ exports.load_home_js_css_etc = (_window) => {
         'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js',
         `${window.location.origin}/assets/js/moment.min.js`
       ]
+      ,
+      firebase: [
+        'https://www.gstatic.com/firebasejs/7.0.0/firebase-app.js',
+        `${window.location.origin}/assets/js/firebase-app-7.0.0.js`
+      ]
+      ,
+      'firebase.auth': [
+        'https://www.gstatic.com/firebasejs/7.0.0/firebase-auth.js',
+        `${window.location.origin}/assets/js/firebase-auth-7.0.0.js`
+      ]
     })
 
     fallback.ready(() => {
