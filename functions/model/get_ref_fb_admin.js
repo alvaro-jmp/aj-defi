6f29bc5fd3ad3088a25d3e0660f41bb831985a21
= <alv.js1011@gmail.com>
2019-11-15 23:24:55 -0400

    modified for work with environment variable and removed dotenv module

--- functions/model/get_ref_fb_admin.js
+++ functions/model/get_ref_fb_admin.js
@@ -1,14 +1,11 @@
 "use strict";
 
-require('dotenv').config();
-
 var admin = require('firebase-admin');
 
 var _require = require('./config_fb'),
     firebaseConfig = _require.firebaseConfig;
 
-var serviceAccount = require('./aj-bank-firebase-adminsdk-service-account.json');
-
+var serviceAccount = JSON.parse(process.env.AJ_BANK_FIREBASE_ADMINSDK_SERVICE_ACCOUNT);
 var app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: firebaseConfig.databaseURL,
--- src/model/get_ref_fb_admin.js
+++ src/model/get_ref_fb_admin.js
@@ -1,13 +1,12 @@
-require('dotenv').config()
 const admin = require('firebase-admin')
 const { firebaseConfig } = require('./config_fb')
-const serviceAccount = require('./aj-bank-firebase-adminsdk-service-account.json')
+const serviceAccount = JSON.parse(process.env.AJ_BANK_FIREBASE_ADMINSDK_SERVICE_ACCOUNT)
 const app = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: firebaseConfig.databaseURL,
-    databaseAuthVariableOverride: {
-        uid: "aj_admin"
-    }
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: firebaseConfig.databaseURL,
+  databaseAuthVariableOverride: {
+    uid: "aj_admin"
+  }
 })
 
 exports.ctx = app
